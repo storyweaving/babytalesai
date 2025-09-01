3548c5e2e4e3dfb9eb93baf944164df490f6cdd0
michael kelly <142418179+storyweaving@users.noreply.github.com>
2025-09-01 09:09:31 -0400

    Update Navbar.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-10">
           <div className="flex items-center space-x-4 min-w-0">
-            <div className="flex-shrink-0 flex items-center space-x-2">
+            <div className="flex-shrink-0 flex items-center space-x-3">
                 <svg className="h-8 w-auto text-green-600 dark:text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/>
                 </svg>
